647af6923aa7a4cc98b36ee3a477cc08c8ad30cd
Franco904 <francostavares2003@gmail.com>
2024-05-16 22:17:11 -0300

    build: ajusta versao do kotlin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,19 +26,24 @@ android {
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
+
     buildFeatures {
         compose = true
     }
+
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
     }
+
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
