8d0d773cb92ea2ad7725c2d5b1547c7ab9be1d30
Franco904 <francostavares2003@gmail.com>
2024-05-20 07:54:41 -0300

    move composable da tela para arquivo separado

--- app/src/main/java/com/example/auth_compose/MainActivity.kt
+++ app/src/main/java/com/example/auth_compose/MainActivity.kt
@@ -3,42 +3,8 @@ package com.example.auth_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
-import androidx.compose.material3.Divider
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.focus.FocusManager
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.auth_compose.ui.theme.AuthcomposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -52,103 +18,8 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AppContent() {
-    val focusManager = LocalFocusManager.current
-
-    Scaffold(topBar = {
-        TopAppBar(
-            title = {
-                Text("Guest list", overflow = TextOverflow.Ellipsis)
-            },
-            colors = TopAppBarDefaults.topAppBarColors(
-                containerColor = MaterialTheme.colorScheme.primary,
-                titleContentColor = MaterialTheme.colorScheme.onPrimary,
-            ),
-            scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(),
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primary)
-                .shadow(elevation = 8.dp)
-        )
-    }) { padding ->
-        Column(horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(
-                    start = 24.dp,
-                    top = padding.calculateTopPadding() + 16.dp,
-                    end = 24.dp,
-                    bottom = 24.dp,
-                )
-                .pointerInput(Unit) {
-                    detectTapGestures(onTap = {
-                        focusManager.clearFocus()
-                    })
-                }) {
-            GuestList(focusManager)
-        }
-    }
-}
-
-@Composable
-fun GuestList(focusManager: FocusManager) {
-    var name by remember { mutableStateOf("") }
-    var names by remember { mutableStateOf(listOf<String>()) }
-
-    Row(
-        verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(bottom = 24.dp)
-    ) {
-        OutlinedTextField(
-            value = name,
-            onValueChange = { newValue -> name = newValue },
-            label = { Text("Name") },
-            modifier = Modifier.weight(1f)
-        )
-        Spacer(modifier = Modifier.width(16.dp))
-        Button(
-            onClick = {
-                if (name.isNotBlank()) {
-                    names = names + name
-                    name = ""
-                    focusManager.clearFocus()
-                }
-            },
-        ) {
-            Text("Add")
-        }
-    }
-    Spacer(modifier = Modifier.height(24.dp))
-    LazyColumn {
-        items(names) { name ->
-            if (name == names.first()) Divider()
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-            ) {
-                Image(
-                    painter = painterResource(R.drawable.baseline_person_outline_24),
-                    contentDescription = null,
-                )
-                Spacer(modifier = Modifier.width(4.dp))
-                Text(
-                    name,
-                    overflow = TextOverflow.Ellipsis,
-                    modifier = Modifier
-                        .padding(16.dp)
-                )
-                Spacer(modifier = Modifier.weight(1f))
-                Button(
-                    onClick = {
-                        names = names.filter { it != name }
-                        focusManager.clearFocus()
-                    },
-                ) {
-                    Text("Remove")
-                }
-            }
-            Divider()
-        }
-    }
 }
 
 @Preview(showBackground = true)
