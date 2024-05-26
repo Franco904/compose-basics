e1c9347497428e09ed01c8fa78f5abe9adb66e1b
Franco904 <francostavares2003@gmail.com>
2024-05-26 15:51:46 -0300

    2nd practice - To-do all done screen

---
+++ app/src/main/java/com/example/auth_compose/ui/composables/TodoAllDoneComposable.kt
@@ -0,0 +1,54 @@
+package com.example.auth_compose.ui.composables
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.auth_compose.R
+import com.example.auth_compose.ui.theme.AuthcomposeTheme
+
+@Composable
+fun TodoAllDoneComposable() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_task_completed),
+            contentDescription = null,
+        )
+        Text(
+            text = stringResource(R.string.todo_all_done_title),
+            fontWeight = FontWeight.Bold,
+            fontSize = 24.sp,
+            modifier = Modifier.padding(
+                top = 24.dp,
+                bottom = 8.dp,
+            )
+        )
+        Text(
+            text = stringResource(R.string.todo_all_done_message),
+            fontSize = 16.sp,
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TodoAllDonePreview() {
+    AuthcomposeTheme {
+        TodoAllDoneComposable()
+    }
+}
