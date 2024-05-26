8a77dfdeb07f9f22838638a0029f124ae72a0bc3
Franco904 <francostavares2003@gmail.com>
2024-05-26 15:51:14 -0300

    1st practice - Compose article

---
+++ app/src/main/java/com/example/auth_compose/ui/composables/ComposeArticleComposable.kt
@@ -0,0 +1,54 @@
+package com.example.auth_compose.ui.composables
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.auth_compose.R
+import com.example.auth_compose.ui.theme.AuthcomposeTheme
+
+@Composable
+fun ComposeArticleComposable() {
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Image(
+            painter = painterResource(R.drawable.bg_compose_background),
+            contentDescription = null,
+            contentScale = ContentScale.FillWidth,
+        )
+        Text(
+            text = stringResource(R.string.compose_article_title),
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp)
+        )
+        Text(
+            text = stringResource(R.string.compose_article_text_1),
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp)
+        )
+        Text(
+            text = stringResource(R.string.compose_article_text_2),
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(16.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ComposeArticlePreview() {
+    AuthcomposeTheme {
+        ComposeArticleComposable()
+    }
+}
