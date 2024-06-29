f853fa26a9a8f52d3f283049c89eee43f51dc14b
Franco904 <francostavares2003@gmail.com>
2024-06-29 16:04:37 -0300

    remove imports

--- app/src/main/java/com/example/auth_compose/MainActivity.kt
+++ app/src/main/java/com/example/auth_compose/MainActivity.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.auth_compose.ui.composables.ArtworkShowcaseComposable
-import com.example.auth_compose.ui.composables.theme.AuthcomposeTheme
 import com.example.auth_compose.ui.composables.theme.ComposeBasicsTheme
-import com.example.auth_compose.ui.woof.WoofDogsComposable
-import com.example.auth_compose.ui.woof.theme.WoofTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,7 +14,7 @@ class MainActivity : ComponentActivity() {
 //                ArtworkShowcaseComposable()
 //            }
 
-            
+
         }
     }
 }
