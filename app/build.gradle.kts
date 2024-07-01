7bd442f7ff5026c136a6839c4c459b21382a14cf
Franco904 <francostavares2003@gmail.com>
2024-07-01 13:23:04 -0300

    adiciona dependencia do view model

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,29 +63,44 @@ composeCompiler {
 }
 
 dependencies {
+    // Android Core
     implementation(libs.androidx.core.ktx)
+
+    // Lifecycle
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+
+    // Compose
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
+
+    // UI
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
+
+    // Material Design
     implementation(libs.androidx.material3)
     implementation(libs.androidx.material.icons.extended)
 
+    // Kotlin
     implementation(libs.kotlin.collections.immutable)
 
+    // Helpers
     implementation(libs.faker)
 
+    // Local Tests
     testImplementation(libs.junit.api)
     testRuntimeOnly(libs.junit.engine)
     testImplementation(libs.kluent)
 
+    // Instrumented Tests
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
 
+    // Other
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 }
