53a993d4cb5537264917326296d79a507d46ae56
Franco904 <francostavares2003@gmail.com>
2024-07-19 09:01:52 -0300

    refactor: remove unscramble game (moved to new git repository)

--- app/src/main/java/com/example/auth_compose/ui/unscramble_game/models/GameState.kt
+++
@@ -1,8 +0,0 @@
-package com.example.auth_compose.ui.unscramble_game.models
-
-enum class GameState {
-    NOT_STARTED,
-    TOPIC_SELECTION,
-    STARTED,
-    FINISHED;
-}
--- app/src/main/java/com/example/auth_compose/ui/unscramble_game/validators/GuessValidator.kt
+++
@@ -1,7 +0,0 @@
-package com.example.auth_compose.ui.unscramble_game.validators
-
-object GuessValidator {
-    fun isValid(guess: String): Boolean {
-        return guess.isNotBlank()
-    }
-}
